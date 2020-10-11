50f7e2a6b8d55c860e78d7f73fe2762f03b22f91
secondsuitor <pete@secondsuitor.com>
2020-10-11 07:23:26 -0700

    add early templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, world!"
+	user = {'username': 'Pietro'}
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Seattle'
+		},
+		{
+			'author': {'username':'Susan'},
+			'body': 'The movie was great!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
