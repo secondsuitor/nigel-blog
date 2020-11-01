b83e2158bf23a71bd671fe63f98dc2c2d0bb3045
secondsuitor <pete@secondsuitor.com>
2020-11-01 09:56:27 -0800

    add logging

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -12,7 +12,7 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	email = StringField('Email', validators=[DataRequired(), Email()])
-	password= PasswordField('Password', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
 	password2 = PasswordField(
 		'Repeat Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Register')
@@ -27,3 +27,7 @@ class RegistrationForm(FlaskForm):
 		if email is not None:
 			raise ValidationError('Please use a different email.')
 
+class EditProfileForm(FlaskForm):
+	username = StringField('Username:', validators=[DataRequired()])
+	about_me = TextAreaField('About me:', validators=[Length(min=0, max=140)])
+	submit = SubmitField('Submit')
