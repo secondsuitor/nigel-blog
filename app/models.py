524913c7733dee201fdeb7732c57c54c84dfb95e
secondsuitor <pete@secondsuitor.com>
2020-11-01 10:39:40 -0800

    add followers to db

--- app/models.py
+++ app/models.py
@@ -4,6 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+
+followers = db.Table('followers',
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
@@ -21,6 +27,11 @@ class User(UserMixin, db.Model):
 	def avatar(self, size):
 		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
 		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+	followed = db.relationship(
+		'User', secondary=followers,
+		primaryjoin=(followers.c.follower_id == id),
+		secondaryjoin=(followers.c.followed_id == id),
+		backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -34,4 +45,3 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
 	return User.query.get(int(id))
-
