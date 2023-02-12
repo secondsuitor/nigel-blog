1bee5ea78495be9ba2b6a4c87ff9a1abcba6dac3
secondsuitor <pete@secondsuitor.com>
2023-02-12 15:25:12 -0800

    add login to routes

--- nigel/routes.py
+++ nigel/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from nigel import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -22,3 +23,8 @@ def index():
 def tester():
     user = {'username': 'tester'}
     return render_template('index.html', user=user)
+
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
