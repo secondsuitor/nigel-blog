c9cf2f94d511899cee9630b5ec7720b1288cdd77
secondsuitor <pete@secondsuitor.com>
2023-02-12 15:20:02 -0800

    templatize index

--- nigel/routes.py
+++ nigel/routes.py
@@ -1,10 +1,24 @@
+from flask import render_template
 from nigel import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    user = {'username': 'peter'}
+    posts  = [
+        {
+            'author': {'username': 'peter'},
+            'body': 'beautiful day'
+        },
+        {
+            'author' : { 'username': 'tester'},
+            'body': 'testing the waters'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/test')
 def tester():
-    return "hello tester"
+    user = {'username': 'tester'}
+    return render_template('index.html', user=user)
