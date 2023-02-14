66f2277b2a0ebb98725fcc71772ca5dd04cfaa4f
secondsuitor <pete@secondsuitor.com>
2023-02-13 20:30:10 -0800

    update base python

--- nigel/routes.py
+++ nigel/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from nigel import app
-from app.forms import LoginForm
+from nigel.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -24,7 +24,11 @@ def tester():
     user = {'username': 'tester'}
     return render_template('index.html', user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remeber_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
