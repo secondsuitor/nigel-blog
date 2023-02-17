789cbcdc71b174158502b6bd9195fb3bf2a2e14f
secondsuitor <pete@secondsuitor.com>
2023-02-16 19:42:57 -0800

    add flask-login

--- nigel/__init__.py
+++ nigel/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -7,5 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from nigel import routes, models
