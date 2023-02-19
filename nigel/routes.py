9e9c90294f3647e8d28c066bc7a483287db68354
secondsuitor <pete@secondsuitor.com>
2023-02-19 13:54:43 -0800

    basic fixes

--- nigel/routes.py
+++ nigel/routes.py
@@ -58,7 +58,7 @@ def registration():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email)
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
