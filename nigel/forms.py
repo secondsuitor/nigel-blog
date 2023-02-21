83894aaf845744817cdb1210ae3fa8d2572cbea6
secondsuitor <pete@secondsuitor.com>
2023-02-20 16:17:13 -0800

    add edit profile functionality

--- nigel/forms.py
+++ nigel/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from nigel.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -25,3 +26,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
--- nigel/routes.py
+++ nigel/routes.py
@@ -5,7 +5,7 @@ from nigel.forms import LoginForm
 from nigel.models import User
 from werkzeug.urls import url_parse
 from nigel import db
-from nigel.forms import RegistrationForm
+from nigel.forms import RegistrationForm, EditProfileForm
 from datetime import datetime
 
 @app.before_request
@@ -82,3 +82,18 @@ def user(username):
         {'author': user, 'body': 'test post #3'}
     ]
     return render_template('user.html', user=user, posts = posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your profile has been updated')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
