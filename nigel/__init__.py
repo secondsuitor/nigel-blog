954e847d4b12fa454b3bd0ce910a7632bce9eb8e
secondsuitor <pete@secondsuitor.com>
2023-05-26 19:27:44 -0700

    add bootstrap to init

--- nigel/__init__.py
+++ nigel/__init__.py
@@ -6,8 +6,10 @@ from flask_migrate import Migrate
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
