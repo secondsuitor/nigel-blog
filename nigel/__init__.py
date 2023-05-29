26299e187f91aa02c6632a989c7d46b772ecb3f9
secondsuitor <pete@secondsuitor.com>
2023-05-29 12:32:40 -0700

    add moment

--- nigel/__init__.py
+++ nigel/__init__.py
@@ -7,6 +7,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -15,6 +16,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+moment = Moment(app)
+
 
 if not app.debug:
     # email error alerts
